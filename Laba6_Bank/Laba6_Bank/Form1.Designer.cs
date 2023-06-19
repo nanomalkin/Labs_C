2eeee70c7dca80b2a74bf13d02ff11084374bdb1
Namalkin <63348391+nanomalkin@users.noreply.github.com>
2023-06-19 15:39:23 +0300

    Add files via upload

---
+++ Laba6_Bank/Laba6_Bank/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Laba6_Bank
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(180, 94);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(88, 28);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Создать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(143, 95);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(31, 22);
+            this.txtSize.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(16, 132);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(247, 16);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Минимальный локальный максимум: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(624, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(176, 252);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Малков Даниил. Вариант 35";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(429, 80);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Дан массив размера N.\r\nНайти минимальный из его локальных максимумов\r\n(локальный " +
+    "максимум — это элемент, который больше любого из\r\nсвоих соседей).\r\nРазмер массив" +
+    "а:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 252);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblResult);
+            this.Name = "Form1";
+            this.Text = "Лабораторная 6. Класс Array";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Laba6_Bank/Laba6_Bank/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba6_Bank
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        public int[] arrX;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtSize.Text, out int N))
+            {
+                int[] arrX = new int[N];
+
+                for (int i = 0; i < N; i++)
+                {
+                    arrX[i] = random.Next(-50, 50); 
+                }
+
+                int min = ArraySearchMinLocalMax(arrX);
+
+                lblResult.Text = "Минимальный локальный максимум: " + min;
+                DisplayArrayInDataGridView(arrX);
+
+            }
+            else
+            {
+                MessageBox.Show("Ошибка ввода размера массива!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private int ArraySearchMinLocalMax(int[] arr)
+        {
+            int min = int.MaxValue;
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                {
+                    if (arr[i] < min)
+                        min = arr[i];
+                }
+            }
+            return min;
+        }
+        private void DisplayArrayInDataGridView(int[] arr)
+        {
+            dataGridView1.Rows.Clear();
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("Column", "Значение");
+
+            dataGridView1.RowCount = arr.Length;
+            int min = ArraySearchMinLocalMax(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = arr[i];
+                if (arr[i] == min)
+                {
+                    dataGridView1.Rows[i].Cells[0].Style.BackColor = Color.Yellow; // Измените цвет выделения по своему усмотрению
+                }
+            }
+        }
+
+    }
+}
